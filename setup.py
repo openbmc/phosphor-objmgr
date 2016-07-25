98994920dce0de13ac7853d885c97488d89d54a1
Brad Bishop <bradleyb@fuzziesquirrel.com>
2016-07-25 09:09:00 -0400

    Promote obmc.mapper to python package
    Enable additional modules in the mapper package namespace.
    
    Change-Id: If2b38eef13b194a6087bb80947884f94bb2a5881
    Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='phosphor-mapper',
       version='1.0',
-      py_modules=['obmc.mapper'],
+      packages=['obmc.mapper'],
       scripts=['phosphor-mapper']
       )
