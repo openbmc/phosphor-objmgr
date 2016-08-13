4af0bb8c8ca1fd9a2f8aa8cec1c41b4feefd6fcb
Brad Bishop <bradleyb@fuzziesquirrel.com>
2016-08-13 14:08:17 -0400

    mapper-cli: rewrite with sdbus
    There are too many instances of the wait/call applications
    spawned during BMC startup.  Re-write using sdbus.
    
    Additionally, use a new utility get-service + busctl
    for making method calls.
    
    Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>
    Change-Id: Ia6fb3b74cb70f93cfd5cc57c1a8b7a9aa2d944c4

--- libmapper/mapper.h
+++ libmapper/mapper.h
@@ -3,6 +3,11 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+typedef struct mapper_async_wait mapper_async_wait;
+void mapper_wait_async_free(mapper_async_wait *);
+
+int mapper_wait_async(sd_bus *, char *[],
+		void (*)(int, void *), void *, mapper_async_wait **);
 int mapper_get_service(sd_bus *conn, const char *obj, char **service);
 #ifdef __cplusplus
 }
