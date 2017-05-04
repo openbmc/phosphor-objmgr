c7a7c45fec58ff593aecce92699b9ee41823d48e
Adriana Kobylak <anoo@us.ibm.com>
2017-05-04 12:35:31 -0500

    Revert "Add mapper-wait-until-removed"
    This reverts commit 9f4c3c7c408a5f84a52416da99d0dd8035f1ae5a.
    
    After further design discussions, the new wait until
    removed implementation should be a new API. Reverting this
    change to put back the original wait interfaces since new
    ones will be created instead of modifying the existing ones.
    
    Change-Id: I605439dcb24c6dca7f394325afc6587eacf417f1
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- libmapper/mapper.h
+++ libmapper/mapper.h
@@ -1,4 +1,3 @@
-#include <stdbool.h>
 #include <systemd/sd-bus.h>
 #include <systemd/sd-event.h>
 
@@ -9,8 +8,7 @@ typedef struct mapper_async_wait mapper_async_wait;
 void mapper_wait_async_free(mapper_async_wait *);
 
 int mapper_wait_async(sd_bus *, sd_event *, char *[],
-		void (*)(int, void *), void *, mapper_async_wait **,
-		bool);
+		void (*)(int, void *), void *, mapper_async_wait **);
 int mapper_get_service(sd_bus *conn, const char *obj, char **service);
 int mapper_get_object(sd_bus *conn, const char *obj, sd_bus_message **reply);
 #ifdef __cplusplus
