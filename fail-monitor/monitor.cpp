6739fd221d7e1d4e04b332d62302c9e140199bda
Matt Spinler <spinler@us.ibm.com>
2017-06-02 12:29:41 -0500

    Add class to monitor for unit failures
    This class has an analyze() method that will
    check the state of a unit, and then stop or start
    a target unit if the state is failed.
    
    The units and the action are passed in via the constructor.
    
    Change-Id: Ibc8e54b8371d2261eb55cce5825c5cee6d214bab
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

---
+++ fail-monitor/monitor.cpp
@@ -0,0 +1,135 @@
+/**
+ * Copyright © 2017 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <phosphor-logging/log.hpp>
+#include "elog-errors.hpp"
+#include "monitor.hpp"
+#include "xyz/openbmc_project/Fail/Monitor/error.hpp"
+
+namespace phosphor
+{
+namespace unit
+{
+namespace failure
+{
+
+using namespace phosphor::logging;
+using namespace sdbusplus::xyz::openbmc_project::Fail::Monitor::Error;
+
+constexpr auto FAILED_STATE = "failed";
+constexpr auto START_METHOD = "StartUnit";
+constexpr auto STOP_METHOD = "StopUnit";
+
+constexpr auto SYSTEMD_SERVICE   = "org.freedesktop.systemd1";
+constexpr auto SYSTEMD_OBJ_PATH  = "/org/freedesktop/systemd1";
+constexpr auto SYSTEMD_INTERFACE = "org.freedesktop.systemd1.Manager";
+constexpr auto SYSTEMD_PROPERTY_INTERFACE = "org.freedesktop.DBus.Properties";
+constexpr auto SYSTEMD_UNIT_INTERFACE = "org.freedesktop.systemd1.Unit";
+
+
+void Monitor::analyze()
+{
+    if (inFailedState(std::move(getSourceUnitPath())))
+    {
+        runTargetAction();
+    }
+}
+
+
+bool Monitor::inFailedState(const std::string&& path)
+{
+    sdbusplus::message::variant<std::string> property;
+
+    auto method = bus.new_method_call(SYSTEMD_SERVICE,
+                                      path.c_str(),
+                                      SYSTEMD_PROPERTY_INTERFACE,
+                                      "Get");
+
+    method.append(SYSTEMD_UNIT_INTERFACE, "ActiveState");
+
+    auto reply = bus.call(method);
+    if (reply.is_method_error())
+    {
+        elog<DBusFailure>(xyz::openbmc_project::Fail::Monitor::DBusFailure::
+                          FAIL("Failed reading ActiveState DBus property"),
+                          xyz::openbmc_project::Fail::Monitor::DBusFailure::
+                          PATH(path.c_str()));
+    }
+
+    reply.read(property);
+
+    auto value = sdbusplus::message::variant_ns::get<std::string>(property);
+    return (value == FAILED_STATE);
+}
+
+
+std::string Monitor::getSourceUnitPath()
+{
+    sdbusplus::message::object_path path;
+
+    auto method = bus.new_method_call(SYSTEMD_SERVICE,
+                                      SYSTEMD_OBJ_PATH,
+                                      SYSTEMD_INTERFACE,
+                                      "GetUnit");
+    method.append(source);
+    auto reply = bus.call(method);
+
+    if (reply.is_method_error())
+    {
+        elog<DBusFailure>(xyz::openbmc_project::Fail::Monitor::DBusFailure::
+                          FAIL("Failed GetUnit DBus method call"),
+                          xyz::openbmc_project::Fail::Monitor::DBusFailure::
+                          PATH(source.c_str()));
+    }
+
+    reply.read(path);
+
+    return static_cast<std::string>(path);
+}
+
+
+void Monitor::runTargetAction()
+{
+    //Start or stop the target unit
+    auto methodCall = (action == Action::start) ?
+                      START_METHOD : STOP_METHOD;
+
+    log<level::INFO>("The source unit is in failed state, "
+                     "running target action",
+                     entry("SOURCE=%s", source.c_str()),
+                     entry("TARGET=%s", target.c_str()),
+                     entry("ACTION=%s", methodCall));
+
+    auto method = this->bus.new_method_call(SYSTEMD_SERVICE,
+                                            SYSTEMD_OBJ_PATH,
+                                            SYSTEMD_INTERFACE,
+                                            methodCall);
+    method.append(target);
+    method.append("replace");
+
+    auto reply = bus.call(method);
+
+    if (reply.is_method_error())
+    {
+        elog<DBusFailure>(xyz::openbmc_project::Fail::Monitor::DBusFailure::
+                          FAIL("Failed to run action on the target unit"),
+                          xyz::openbmc_project::Fail::Monitor::DBusFailure::
+                          PATH(target.c_str()));
+    }
+}
+
+}
+}
+}
