403cc548d794636c2a1154fc993094415e0461ae
Adriana Kobylak <anoo@us.ibm.com>
2017-12-11 13:32:10 -0600

    bindings.py: Check for "*.DBus.Error.UnknownInterface"
    The GET function was checking for "org.freedesktop.UnknownInterface"
    which is thrown by the openbmc python dbus implementation, but
    sdbus uses "org.freedesktop.DBus.Error.UnknownInterface", so add
    checking for the sdbus error one which has been added via a new define.
    
    Also replace get_dbus_message with get_dbus_name because the name
    is the one that contains the error name, and the message contains
    the full description.
    
    Resolves openbmc/openbmc#2463
    
    Change-Id: I8721e7b262dbc8f4ebae9a8e30074bdf6a53b025
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- obmc/mapper/bindings.py
+++ obmc/mapper/bindings.py
@@ -82,7 +82,11 @@ class Mapper:
             return f(*a)
         except dbus.exceptions.DBusException, e:
             if obmc.dbuslib.enums.DBUS_UNKNOWN_INTERFACE in \
-                    e.get_dbus_message():
+                    e.get_dbus_name():
+                # interface doesn't have any properties
+                return None
+            if obmc.dbuslib.enums.DBUS_UNKNOWN_INTERFACE_ERROR in \
+                    e.get_dbus_name():
                 # interface doesn't have any properties
                 return None
             if obmc.dbuslib.enums.DBUS_UNKNOWN_METHOD == e.get_dbus_name():
