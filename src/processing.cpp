efdf27c6dd904c39081e440941fb649b02474e85
Andrew Geissler <geissonator@yahoo.com>
2019-02-04 12:19:41 -0600

    unit-test: Test need_to_introspect function
    Testing: Verified 100% code coverage of processing.cpp
    
    Change-Id: I5ebdebe3fdcecbf250a23754f8b5c7db81bfaaa3
    Signed-off-by: Andrew Geissler <geissonator@yahoo.com>

--- src/processing.cpp
+++ src/processing.cpp
@@ -20,3 +20,19 @@ bool getWellKnown(
     wellKnown = it->second;
     return true;
 }
+
+bool needToIntrospect(const std::string& processName,
+                      const WhiteBlackList& whiteList,
+                      const WhiteBlackList& blackList)
+{
+    auto inWhitelist =
+        std::find_if(whiteList.begin(), whiteList.end(),
+                     [&processName](const auto& prefix) {
+                         return boost::starts_with(processName, prefix);
+                     }) != whiteList.end();
+
+    // This holds full service names, not prefixes
+    auto inBlacklist = blackList.find(processName) != blackList.end();
+
+    return inWhitelist && !inBlacklist;
+}
