646053c150ba35c7595743e79b6232a16f9742eb
Andrew Geissler <geissonator@yahoo.com>
2019-02-21 13:44:16 -0600

    unit-test: Move checkAssociationEndpointRemoves
    Make it easier to unit test and continue reduction of main.cpp
    
    Change-Id: Id4f4c4fc1e3928f1b600555c6dbe05f651fffce5
    Signed-off-by: Andrew Geissler <geissonator@yahoo.com>

--- src/associations.hpp
+++ src/associations.hpp
@@ -79,3 +79,29 @@ void removeAssociationEndpoints(
     const std::string& owner,
     const boost::container::flat_set<std::string>& endpointsToRemove,
     AssociationInterfaces& assocInterfaces);
+
+/** @brief Check and remove any changed associations
+ *
+ * Based on the latest values of the org.openbmc.Associations.associations
+ * property, passed in via the newAssociations param, check if any of the
+ * paths in the xyz.openbmc_project.Association.endpoints D-Bus property
+ * for that association need to be removed.  If the last path is removed
+ * from the endpoints property, remove that whole association object from
+ * D-Bus.
+ *
+ * @param[in] sourcePath         - Path of the object that contains the
+ *                                 org.openbmc.Associations
+ * @param[in] owner              - The Dbus service having it's associatons
+ *                                 changed
+ * @param[in] newAssociations    - New associations to look at for change
+ * @param[in,out] objectServer   - sdbus system object
+ * @param[in,out] assocOwners    - Owners of associations
+ * @param[in,out] assocInterfaces - Associations endpoints
+ *
+ * @return Void, objectServer and assocOwners updated if needed
+ */
+void checkAssociationEndpointRemoves(
+    const std::string& sourcePath, const std::string& owner,
+    const AssociationPaths& newAssociations,
+    sdbusplus::asio::object_server& objectServer,
+    AssociationOwnersType& assocOwners, AssociationInterfaces& assocInterfaces);
