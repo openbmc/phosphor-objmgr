90ef4590833ad98a04173311555d4551095d329b
Kun Yi <kunyi731@gmail.com>
2019-03-27 09:23:54 -0700

    cleanup: Remove Python code and makefiles
    Remove Python code and build files now that we switched to C++ mapper
    
    Signed-off-by: Kun Yi <kunyi731@gmail.com>
    Change-Id: Ia8832eaae52ed85b4c1620a5d0db181e6b744ac7

--- obmc/mapper/__init__.py
+++
@@ -1,6 +0,0 @@
-import obmc.mapper.bindings
-from obmc.mapper.bindings import Mapper
-from obmc.mapper.bindings import MAPPER_NAME
-from obmc.mapper.bindings import MAPPER_IFACE
-from obmc.mapper.bindings import MAPPER_PATH
-from obmc.mapper.bindings import MAPPER_NOT_FOUND
