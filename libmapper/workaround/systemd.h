45e764a25f37c6a175ff7922cf8da1faa9423c8c
Brad Bishop <bradleyb@fuzziesquirrel.com>
2021-08-02 23:46:30 -0400

    mapper client: drop patch build configuration
    Support for --enable-unpatched-systemd is dropped because
    sd_bus_message_append_cmdline is no longer used after
    2d04816fb5a1811694272818ec41b5364ce3c691.
    
    Change-Id: I926ce4a0a1077ea99c14098230c2ee171ec13781
    Fixes: 2d04816fb5a1811694272818ec41b5364ce3c691
    Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

--- libmapper/workaround/systemd.h
+++
@@ -1,9 +0,0 @@
-#ifdef IS_UNPATCHED_SYSTEMD
-#include <systemd/sd-bus.h>
-
-int sd_bus_message_append_cmdline(sd_bus_message* m, const char* signature,
-                                  char*** x)
-{
-    return 0;
-}
-#endif
