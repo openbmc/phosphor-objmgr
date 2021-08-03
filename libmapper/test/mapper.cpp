a669a50a94f428ef380a44d3464026e44eb95f17
Brad Bishop <bradleyb@fuzziesquirrel.com>
2021-08-03 16:01:43 -0400

    libmapper: fix logic bug
    sarraylen returns the number of strings and does not include the
    trailing null pointer.  This results in sarraydup creating arrays of
    strings where the array is not null pointer terminated (the strings
    themselves _are_ \0 terminated), which in turn causes random data to be
    passed to free.
    
    The bug was found using static analysis and inspection would indicate
    exposure only exists on error and shutdown paths, which might be a hint
    as to how this has been lurking for as long as it has.
    
    Change-Id: Ie5e5b6cdfb7832c84037ff039b41082fc7d20b61
    Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

---
+++ libmapper/test/mapper.cpp
@@ -0,0 +1,22 @@
+#include "../internal.h"
+#include "utils.h"
+
+#include <gtest/gtest.h>
+
+TEST(TestSarray, Dup)
+{
+    auto a = generate_test_sarray(3);
+
+    auto size = sarraylen(a);
+    EXPECT_EQ(size, 3);
+
+    auto b = sarraydup(a);
+    size_t i;
+
+    for (i = 0; i < 4; i++)
+    {
+        EXPECT_STREQ(a[i], b[i]);
+    }
+    sarrayfree(a);
+    sarrayfree(b);
+}
---
+++ libmapper/test/utils.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <stddef.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+char** generate_test_sarray(size_t);
+#ifdef __cplusplus
+}
+#endif
