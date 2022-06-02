e0d9b0de4b6b1787e0a838b71815e472bd8803f5
Brad Bishop <bradleyb@fuzziesquirrel.com>
2022-06-02 19:56:23 -0400

    mapper: remove service deny list feature
    This feature has existed for years, but remains unused.  Drop the dead
    code.
    
    Change-Id: I1b0b6f7fee0da30a3e36e1d151e70e1039fe2c7b
    Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

--- src/test/need_to_introspect.cpp
+++ src/test/need_to_introspect.cpp
@@ -6,38 +6,16 @@
 TEST(NeedToIntrospect, PassEmptyName)
 {
     AllowDenyList allowList;
-    AllowDenyList denyList;
     std::string processName;
 
-    EXPECT_FALSE(needToIntrospect(processName, allowList, denyList));
+    EXPECT_FALSE(needToIntrospect(processName, allowList));
 }
 
 // Verify if name is on allowlist, true is returned
 TEST(NeedToIntrospect, ValidAllowListName)
 {
     AllowDenyList allowList = {"xyz.openbmc_project"};
-    AllowDenyList denyList;
     std::string processName = "xyz.openbmc_project.State.Host";
 
-    EXPECT_TRUE(needToIntrospect(processName, allowList, denyList));
-}
-
-// Verify if name is on denylist, false is returned
-TEST(NeedToIntrospect, ValidDenyListName)
-{
-    AllowDenyList allowList;
-    AllowDenyList denyList = {"xyz.openbmc_project.State.Host"};
-    std::string processName = "xyz.openbmc_project.State.Host";
-
-    EXPECT_FALSE(needToIntrospect(processName, allowList, denyList));
-}
-
-// Verify if name is on allowlist and denylist, false is returned
-TEST(NeedToIntrospect, ValidAllowAndDenyListName)
-{
-    AllowDenyList allowList = {"xyz.openbmc_project"};
-    AllowDenyList denyList = {"xyz.openbmc_project.State.Host"};
-    std::string processName = "xyz.openbmc_project.State.Host";
-
-    EXPECT_FALSE(needToIntrospect(processName, allowList, denyList));
+    EXPECT_TRUE(needToIntrospect(processName, allowList));
 }
