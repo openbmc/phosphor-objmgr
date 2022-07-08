b7d0c71b010fa5fdd9bd1e28f0f72a95818bf4a2
Brandon Kim <brandonkim@google.com>
2022-07-08 09:57:22 -0700

    main: Match getSubTrees* to GetAncestors pattern
    getSubTrees, getSubTreePaths and GetAncestors have a code snippet that's
    very similar. Unify them so that they are easier to read.
    
    For the ret vector being declared further down, it's better practice to
    declare variables closer to where it's going to be used since
    we wouldn't have needed ret if we error out early.
    
    Tested: The code is only syntactically different. Tested with the
    following commit in more depth.
    
    Signed-off-by: Brandon Kim <brandonkim@google.com>
    Change-Id: Ib449ab4ffab502c801040cf6bce28b30507d8e0b

--- src/main.cpp
+++ src/main.cpp
@@ -470,7 +470,13 @@ std::vector<InterfaceMapType::value_type>
     for (const auto& objectPath : interfaceMap)
     {
         const auto& thisPath = objectPath.first;
-        if (boost::starts_with(reqPath, thisPath) && (reqPath != thisPath))
+
+        if (reqPath == thisPath)
+        {
+            continue;
+        }
+
+        if (boost::starts_with(reqPath, thisPath))
         {
             if (interfaces.empty())
             {
@@ -542,7 +548,6 @@ std::vector<InterfaceMapType::value_type>
     }
     // Interfaces need to be sorted for intersect to function
     std::sort(interfaces.begin(), interfaces.end());
-    std::vector<InterfaceMapType::value_type> ret;
 
     if (boost::ends_with(reqPath, "/"))
     {
@@ -554,6 +559,7 @@ std::vector<InterfaceMapType::value_type>
             ResourceNotFound();
     }
 
+    std::vector<InterfaceMapType::value_type> ret;
     for (const auto& objectPath : interfaceMap)
     {
         const auto& thisPath = objectPath.first;
@@ -597,7 +603,6 @@ std::vector<std::string> getSubTreePaths(const InterfaceMapType& interfaceMap,
     }
     // Interfaces need to be sorted for intersect to function
     std::sort(interfaces.begin(), interfaces.end());
-    std::vector<std::string> ret;
 
     if (boost::ends_with(reqPath, "/"))
     {
@@ -609,6 +614,7 @@ std::vector<std::string> getSubTreePaths(const InterfaceMapType& interfaceMap,
             ResourceNotFound();
     }
 
+    std::vector<std::string> ret;
     for (const auto& objectPath : interfaceMap)
     {
         const auto& thisPath = objectPath.first;
