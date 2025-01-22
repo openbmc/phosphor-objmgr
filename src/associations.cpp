2690ff018de8b38390c24b6349fc3de896b87e93
Benjamin Fair <benjaminfair@google.com>
2025-01-22 22:07:23 +0000

    associations: fix leak in assocMaps.ifaces
    New entries get added to ifaces whenever an object with associations is
    detected, but the entries are never removed even if all the endpoints
    are gone, leading to a memory leak.
    
    Change-Id: Ide5439e4eaab7c70fb0331d59d59753905f45066
    Signed-off-by: Benjamin Fair <benjaminfair@google.com>
    Signed-off-by: Willy Tu <wltu@google.com>

--- src/associations.cpp
+++ src/associations.cpp
@@ -10,9 +10,13 @@ void updateEndpointsOnDbus(sdbusplus::asio::object_server& objectServer,
                            const std::string& assocPath,
                            AssociationMaps& assocMaps)
 {
-    auto& iface = assocMaps.ifaces[assocPath];
-    auto& i = std::get<ifacePos>(iface);
-    auto& endpoints = std::get<endpointsPos>(iface);
+    auto iface = assocMaps.ifaces.find(assocPath);
+    if (iface == assocMaps.ifaces.end())
+    {
+        return;
+    }
+    auto& i = std::get<ifacePos>(iface->second);
+    auto& endpoints = std::get<endpointsPos>(iface->second);
 
     // If the interface already exists, only need to update
     // the property value, otherwise create it
@@ -22,20 +26,18 @@ void updateEndpointsOnDbus(sdbusplus::asio::object_server& objectServer,
         {
             objectServer.remove_interface(i);
             i = nullptr;
+            assocMaps.ifaces.erase(iface);
         }
         else
         {
             i->set_property("endpoints", endpoints);
         }
     }
-    else
+    else if (!endpoints.empty())
     {
-        if (!endpoints.empty())
-        {
-            i = objectServer.add_interface(assocPath, xyzAssociationInterface);
-            i->register_property("endpoints", endpoints);
-            i->initialize();
-        }
+        i = objectServer.add_interface(assocPath, xyzAssociationInterface);
+        i->register_property("endpoints", endpoints);
+        i->initialize();
     }
 }
 
