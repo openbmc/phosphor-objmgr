7f900ff92c4a1bd7727361ff0fb25bdeb5ff0f97
Brad Bishop <bradbish@qti.qualcomm.com>
2025-07-29 15:18:44 -0400

    tidy: fix performance-avoid-endl
    Tested by building the code.
    
    Change-Id: I3de027d34b3e91bc08923b47bda16b0b0f4b34b4
    Signed-off-by: Brad Bishop <bradbish@qti.qualcomm.com>

--- src/test/util/debug_output.hpp
+++ src/test/util/debug_output.hpp
@@ -7,66 +7,66 @@
 void dumpAssociationOwnersType(AssociationOwnersType& assocOwners)
 {
     using namespace std;
-    cout << "##### AssociationOwnersType #####" << endl;
+    cout << "##### AssociationOwnersType #####\n";
     for (const auto& i : assocOwners)
     {
-        cout << "------------------------------------" << endl;
-        cout << setw(15) << left << "OBJ PATH:" << i.first << endl;
+        cout << "------------------------------------\n";
+        cout << setw(15) << left << "OBJ PATH:" << i.first << '\n';
 
         for (const auto& j : i.second)
         {
-            cout << setw(16) << left << "DBUS SERVICE:" << j.first << endl;
+            cout << setw(16) << left << "DBUS SERVICE:" << j.first << '\n';
 
             for (const auto& k : j.second)
             {
-                cout << setw(17) << left << "ASSOC PATH:" << k.first << endl;
+                cout << setw(17) << left << "ASSOC PATH:" << k.first << '\n';
 
                 for (const auto& l : k.second)
                 {
-                    cout << setw(18) << left << "ENDPOINT:" << l << endl;
+                    cout << setw(18) << left << "ENDPOINT:" << l << '\n';
                 }
             }
         }
-        cout << "------------------------------------" << endl;
+        cout << "------------------------------------\n";
     }
 }
 
 void dumpAssociationInterfaces(AssociationInterfaces& assocInterfaces)
 {
     using namespace std;
-    cout << "##### AssociationInterfaces #####" << endl;
+    cout << "##### AssociationInterfaces #####\n";
     for (auto i : assocInterfaces)
     {
-        cout << "------------------------------------" << endl;
-        cout << setw(15) << left << "OBJ PATH:" << i.first << endl;
+        cout << "------------------------------------\n";
+        cout << setw(15) << left << "OBJ PATH:" << i.first << '\n';
         auto intfEndpoints = std::get<endpointsPos>(i.second);
 
         for (const auto& k : intfEndpoints)
         {
-            cout << setw(16) << left << "ENDPOINTS:" << k << endl;
+            cout << setw(16) << left << "ENDPOINTS:" << k << '\n';
         }
-        cout << "------------------------------------" << endl;
+        cout << "------------------------------------\n";
     }
 }
 
 void dumpInterfaceMapType(InterfaceMapType& intfMap)
 {
     using namespace std;
-    cout << "##### interfaceMapType #####" << endl;
+    cout << "##### interfaceMapType #####\n";
     for (const auto& i : intfMap)
     {
-        cout << "------------------------------------" << endl;
-        cout << setw(15) << left << "OBJ PATH:" << i.first << endl;
+        cout << "------------------------------------\n";
+        cout << setw(15) << left << "OBJ PATH:" << i.first << '\n';
 
         for (const auto& j : i.second)
         {
-            cout << setw(16) << left << "DBUS SERVICE:" << j.first << endl;
+            cout << setw(16) << left << "DBUS SERVICE:" << j.first << '\n';
 
             for (const auto& k : j.second)
             {
-                cout << setw(18) << left << "INTERFACE:" << k << endl;
+                cout << setw(18) << left << "INTERFACE:" << k << '\n';
             }
         }
     }
-    cout << "------------------------------------" << endl;
+    cout << "------------------------------------\n";
 }
